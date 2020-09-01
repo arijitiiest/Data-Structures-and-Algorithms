c80bbf1801067d342c55940a017feb1cae48dc1f
arijitiiest <1999arijitchoudhury@gmail.com>
2020-09-01 21:07:22 +0530

    repeat and missing number

---
+++ Array/09.repeat_and_missing_number_array.cpp
@@ -0,0 +1,36 @@
+/*
+    You are given a read only array of n integers from 1 to n.
+    Each integer appears exactly once except A which appears twice and B which is missing.
+    Return A and B.
+
+    Input:[3 1 2 5 3] 
+    Output:[3, 4] 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void findNumbers(int arr[], int n)
+{
+    long long sum = 0, sq_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        sq_sum += arr[i] * arr[i];
+    }
+
+    long long ab = sum - (n * (n + 1)) / 2;                  // a - b
+    long long a2b2 = sq_sum - n * (n + 1) * (2 * n + 1) / 6; // a^2 - b^2
+
+    long long a = (ab + (a2b2 / ab)) / 2;
+    long long b = (a2b2 / ab) - a;
+
+    cout << a << " " << b << "\n";
+}
+
+int main()
+{
+    int arr[] = {3, 1, 2, 5, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    findNumbers(arr, n);
+}
