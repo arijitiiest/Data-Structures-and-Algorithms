b657f21e815a2fdce3cf921f993d38f9ffc099d7
arijitiiest <1999arijitchoudhury@gmail.com>
2020-09-03 15:56:54 +0530

    median of array

---
+++ Searching/07.median_of_array.cpp
@@ -0,0 +1,48 @@
+/*
+    There are two sorted arrays A and B of size m and n respectively.
+    Find the median of the two sorted arrays ( The median of the array formed by merging both the arrays ).
+    The overall run time complexity should be O(log (m+n)).
+
+    Input:
+        A : [1 4 5]
+        B : [2 3]
+    Output:
+        3
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+double findMedianSortedArrays(const vector<int> &A, const vector<int> &B)
+{
+    int m = A.size(), n = B.size();
+
+    if (m < n)
+        return findMedianSortedArrays(B, A);
+
+    int l = 0, h = 2 * n;
+
+    while (l <= h)
+    {
+        int mid = l + (h - l) / 2;
+        int mid1 = m + n - mid;
+
+        double L1 = (mid1 == 0) ? INT_MIN : A[(mid1 - 1) / 2];
+        double L2 = (mid == 0) ? INT_MIN : B[(mid - 1) / 2];
+        double R1 = (mid1 == m * 2) ? INT_MAX : A[(mid1) / 2];
+        double R2 = (mid == n * 2) ? INT_MAX : B[(mid) / 2];
+
+        if (L1 > R2)
+            l = mid + 1;
+        else if (L2 > R1)
+            h = mid - 1;
+        else
+            return (max(L1, L2) + min(R1, R2)) / 2;
+    }
+}
+
+int main()
+{
+    vector<int> A{1, 4, 5}, B{2, 3};
+    cout << findMedianSortedArrays(A, B) << endl;
+}
