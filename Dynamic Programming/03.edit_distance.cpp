9ae3c1789cf03fa2892e306e7169aa5382e7a022
arijitiiest <1999arijitchoudhury@gmail.com>
2020-11-18 09:54:37 +0530

    fixed

--- Dynamic Programming/03.edit_distance.cpp
+++ Dynamic Programming/03.edit_distance.cpp
@@ -28,9 +28,9 @@ int editDistance(string A, string B)
     int dp[n + 1][m + 1];
 
     for (int i = 0; i <= n; i++)
-        dp[i][0] = 0;
+        dp[i][0] = i;
     for (int j = 0; j <= m; j++)
-        dp[0][j] = 0;
+        dp[0][j] = j;
 
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
